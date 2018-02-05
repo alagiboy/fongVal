198e3dcd77571cc25597c2d773a0e2f8aac82c4c
Abdou Ndure <abdou.ndure@7summitsinc.com>
2018-02-05 10:19:33 -0600

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,42 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database-deprecated';
+
+import { StatusesComponent } from './statuses/statuses.component';
+import { FormsModule } from '@angular/forms';
+import { StatusesService } from './statuses/statuses.service';
+
+// Define the firebase database configuration keys, get it from your Firebase application console
+export const firebaseConfig = {
+  apiKey: "AIzaSyDvhnIvf8F7eogrjOPexKq91ZMUluUSbTQ",
+  authDomain: "fongval-e7bea.firebaseapp.com",
+  databaseURL: "https://fongval-e7bea.firebaseio.com",
+  projectId: "fongval-e7bea",
+  storageBucket: "fongval-e7bea.appspot.com",
+  messagingSenderId: "1018384034569"
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    SidebarComponent,
+    StatusesComponent
   ],
   imports: [
-    BrowserModule
+    FormsModule,
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
-  providers: [],
+  providers: [
+    StatusesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
